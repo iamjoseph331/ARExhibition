bc9d4aff1c6566668d29ab6d7441e22d8f2730f0
Jostein <jostein.brevik@gmail.com>
2017-12-06 11:43:48 +0800

    Git init yay

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerMove : NetworkBehaviour
+{
+	private Slider rotationSlider;
+	void Start(){
+		rotationSlider = GameObject.Find ("rotationSlider").GetComponent<Slider>();
+		GameObject.Find ("rotationSlider").GetComponent<rotationSlider>().addObject (this.gameObject);
+	}
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+
+	public void RotateMyObject(float value)
+	{
+		transform.rotation = Quaternion.Euler(value * 360, 0, 90);
+	}
+}
---
+++ Assets/rotationSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotationSlider : MonoBehaviour {
+
+	GameObject rotationObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void valueChanged(float value){
+		float realValue = this.gameObject.GetComponent<Slider> ().value; // Value passed is set to 0, need to find real one
+		if (rotationObject) {
+			rotationObject.GetComponent<PlayerMove>().RotateMyObject (realValue);
+		}
+	}
+
+	public void addObject(GameObject obj){
+		rotationObject = obj;
+	}
+}
